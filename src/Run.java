26515a6b77beabe1d3d07f7f5c2309df381d7f2b
Pavel Gubanov <cofer@yandex.ru>
2017-02-13 20:23:21 +0400

    init

---
+++ src/Run.java
@@ -0,0 +1,11 @@
+import view.Gui;
+
+/**
+ * Created by Gubanov Pavel on 25.11.16.
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        new Gui();
+    }
+}
---
+++ src/model/Singleton.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * Created by Gubanov Pavel on 06.01.17.
+ */
+enum Singleton {
+    INSTANCE;
+
+    private final Initializer init;
+
+    Singleton() {
+        init = new Initializer();
+    }
+
+    public Initializer getInit() {
+        return init;
+    }
+}
